97530312cd172f9a1cb752be2871b4ed1c3b2c5a
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-10-10 17:43:31 +0000

    displayed name using jsx in react, called jsx functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Space',
+  lastName: 'Bun'
+};
+
+const element = (
+  <h1>hello {formatName(user)}</h1>
+)
+root.render(element)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
