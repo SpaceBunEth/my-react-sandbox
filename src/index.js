1c128dfcc813d9386cff6275ab0b62960bcfb054
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-10-10 17:59:27 +0000

    parent and children html elements written in jsx

--- src/index.js
+++ src/index.js
@@ -6,28 +6,37 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
 
-const user = {
-  firstName: 'Space',
-  lastName: 'Bun'
-};
+// const user = {
+//   firstName: 'Space',
+//   lastName: 'Bun'
+// };
 
-function getGreeting(user) {
-  if(user) {
-    return <h1>Hello, {formatName(user)}!</h1>;
-  }
-  return <h1>hello, stranger</h1>;
-}
+// function getGreeting(user) {
+//   if(user) {
+//     return <h1>Hello, {formatName(user)}!</h1>;
+//   }
+//   return <h1>hello, stranger</h1>;
+// }
 
-const element = (
-  <h1>{getGreeting(user)}</h1>
-)
+// const element = (
+//   <h1>{getGreeting(user)}</h1>
+// )
 
-root.render(element)
+// root.render(element)
 
+const element2 = (
+  <div>
+    <h1>Hello!</h1>
+    <h2>Good to see you here, SPACEBUN!</h2>
+  </div>
+
+);
+
+root.render(element2)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
